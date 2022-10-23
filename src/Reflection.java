87435ea77b0e2626cf77852a768976b7b0bec018
agatha <agathakrivokon@gmail.com>
2022-10-23 14:00:19 +0300

    first commit

---
+++ src/Reflection.java
@@ -0,0 +1,27 @@
+import java.lang.annotation.Annotation;
+import java.lang.reflect.*;
+class Testers {
+    @Test(a=1, b=5)
+    public void test(int a, int b){
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
+public class Reflection{
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        final Class<?> cls = Testers.class;
+        Method[] methods = cls.getMethods();
+        int a1 = 0;
+        int b1 = 0;
+        for (Method method : methods){
+            if (method.isAnnotationPresent(Test.class)){
+                a1 = method.getAnnotation(Test.class).a();
+                b1 = method.getAnnotation(Test.class).b();
+            }
+        }
+        Testers testing = new Testers();
+        testing.test(a1, b1);
+    }
+}
+
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+import java.lang.annotation.*;
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Test {
+    int a();
+    int b();
+}
